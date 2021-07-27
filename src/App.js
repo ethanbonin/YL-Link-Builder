f92b2fe7eb265f351d8e527412e87c4f1aa04831
Christopher Ethan Bonin <ethanbonin@gmail.com>
2021-07-27 17:11:58 -0500

    Finished the initial UI and UX for the link builder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function YoungLivingFinalUrlComponent(props) {
+  const url = props.url
+  const memberId = props.memberId
+
+  console.log(props)
+
+  if (!url || !memberId) {
+    return null;
+  }
+
+  const handleButtonClick = () => {
+    const el = document.createElement('textarea');
+    el.value = `${url}?sponsorid=${memberId}&enrollerid=${memberId}`;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    alert("Linked copied to clipboard")
+  }
+
+  return (
+      <label>
+        Click to copy!
+        <br />
+        <button onClick={handleButtonClick}>
+          {`${url}?sponsorid=${memberId}&enrollerid=${memberId}`}
+        </button>
+      </label>
+  )
+}
+
 
 function App() {
+  const [youngLivingUrl, setYoungLivingUrl] = useState("")
+  const [memberId, setMemberId] = useState("")
+
+  const handleYlUrlInput = (e) => {
+    setYoungLivingUrl(e.target.value)
+  }
+
+  const handleMemberInput = (e) => {
+    setMemberId(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>YL Link Builder</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Paste in the young living product link, then your YL MemberID.
+          <br/>
+          It will auto generate a link for you
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+
+          <label>
+            Young Living Product Link:
+            <br/>
+            <input type="text" name="youngliving-product-link" onChange={handleYlUrlInput} />
+          </label>
+
+          <br/>
+
+          <label>
+            Your Member/Enroller id:
+            <br/>
+            <input type="text" name="memberid" onChange={handleMemberInput}/>
+          </label>
+
+        </form>
+
+        <br />
+
+        <YoungLivingFinalUrlComponent url={youngLivingUrl} memberId={memberId} />
       </header>
     </div>
   );
